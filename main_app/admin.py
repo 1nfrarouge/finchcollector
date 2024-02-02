40e3fa25fe57463f10c51a0a7a459c14085bb4f5
1nfrarouge <samantha.yc.lim@gmail.com>
2024-02-02 01:17:47 -0500

    Completed Finch Collector Lab Part 4 excercises

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Finch
+from .models import Cat, Feeding
 
 # Register your models here.
 admin.site.register(Finch)
+admin.site.register(Feeding)
